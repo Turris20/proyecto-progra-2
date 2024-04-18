df3b514a6c571b3919a46edd3889c28bbd019d0f
Turris20 <148021540+Turris20@users.noreply.github.com>
2024-04-17 21:25:14 -0600

    Update page.js

--- src/app/registro/page.js
+++ src/app/registro/page.js
@@ -1,16 +1,30 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
-  
-
+import Head from "next/head"; // Importar correctamente desde 'next/head'
 
 export default function Page() {
     return (
-    <main>
-      <Link href={'/'} className={styles.linkbotonvolver}>
-      <span>&#8592;</span>  volver</Link>
-      <center><h1>Página de registro</h1></center>
-    </main>
-      
+        <>
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Registro de Usuario</title>
+            </Head>
+            <main>
+                <div className={styles.container}>
+                    <h2 className={styles.h2}>Registro de Usuario</h2>
+                    <form id="registroForm" action="#" method="post">
+                        <label className={styles.label} htmlFor="nombre">Nombre:</label>
+                        <input className={styles.inputText} type="text" id="nombre" name="nombre" required />
+                        <label className={styles.label} htmlFor="email">Correo Electrónico:</label>
+                        <input className={styles.inputEmail} type="email" id="email" name="email" required />
+                        <label className={styles.label} htmlFor="password">Contraseña:</label>
+                        <input className={styles.inputPassword} type="password" id="password" name="password" required />
+                        <input className={styles.inputSubmit} type="submit" value="Registrar" />
+                    </form>
+                </div>
+            </main>
+        </>
     );
-  }
+}
