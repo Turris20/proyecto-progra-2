566acbcf49ade0d04ebb5b05ab18cccc4ad093d0
dania2222 <148015745+dania2222@users.noreply.github.com>
2024-04-21 20:01:44 -0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,32 @@
-import Image from "next/image";
+import Head from "next/head";
 import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.loginContainer}>
-        <h1>Bienvenido</h1>
-        <p>¿Qué desea realizar?</p>
-        <center>
-        <ul className={styles.listaordenada}>
-        <li><Link href={'/iniciodesesion'} className={styles.linkboton}>
-          Inicia sesión
-        </Link></li>
-        <br></br>
-        <li><Link href={'/registro'} className={styles.linkboton} > 
-          Regístrate
-        </Link></li>
-        </ul>
-        </center>
-      </div>
-      
-      
-    </main>
+    <>
+      <Head>
+        <title>HealthSync: Tu salud, siempre sincronizada</title>
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Bienvenido a HealthSync</h1>
+            <p className={styles.subtitle}>Tu salud, siempre sincronizada.</p>
+            <div className={styles.buttonsContainer}>
+              <Link href="/iniciodesesion" passHref>
+                <button className={styles.button}>Inicia sesión</button>
+              </Link>
+              <Link href="/registro" passHref>
+                <button className={styles.button}>Regístrate</button>
+              </Link>
+            </div>
+          </div>
+          <div className={styles.imageContainer}>
+            <img src="/foto.jpg" className={styles.image} alt="Imagen de fondo" />
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
