87f62d19ad774135860215cc8acbeadb02af487d
dania2222 <148015745+dania2222@users.noreply.github.com>
2024-04-21 20:07:59 -0600

    Update page.js

--- src/app/iniciodesesion/page.js
+++ src/app/iniciodesesion/page.js
@@ -1,7 +1,6 @@
 'use client'
-import Image from "next/image";
-import styles from "./page.module.css";
 import { useState } from "react";
+import styles from "./page.module.css";
 
 export default function Home() {
   const [username, setUsername] = useState("");
@@ -18,34 +17,36 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <div className={styles.loginContainer}>
-        {isLoggedIn ? (
-          <>
-            <h1>Bienvenido, {username}!</h1>
-            <p>Aquí va el contenido protegido por inicio de sesión.</p>
-          </>
-        ) : (
-          <div className={styles.formWrapper}>
-            <h1 className={styles.formTitle}>Iniciar Sesión</h1>
-            <div className={styles.form}>
-              <input
-                type="text"
-                placeholder="Nombre de usuario"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className={styles.inputField}
-              />
-              <input
-                type="password"
-                placeholder="Contraseña"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className={styles.inputField}
-              />
-              <button onClick={handleLogin} className={styles.loginButton}>Ingresar</button>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          {isLoggedIn ? (
+            <>
+              <h1>Bienvenido, {username}!</h1>
+              <p>Aquí va el contenido protegido por inicio de sesión.</p>
+            </>
+          ) : (
+            <div className={styles.formWrapper}>
+              <h1 className={styles.formTitle}>Iniciar Sesión</h1>
+              <div className={styles.form}>
+                <input
+                  type="text"
+                  placeholder="Nombre de usuario"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className={styles.inputField}
+                />
+                <input
+                  type="password"
+                  placeholder="Contraseña"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className={styles.inputField}
+                />
+                <button onClick={handleLogin} className={styles.loginButton}>Ingresar</button>
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </main>
   );
